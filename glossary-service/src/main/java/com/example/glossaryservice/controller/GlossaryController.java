892a4ea0c66e30e5432fdd0cd4fa17d95fc54d59
Maria Sanchez <msanchez157@gmail.com>
2020-04-02 10:23:24 -0400

    Created controller for glossary-service

--- glossary-service/src/main/java/com/example/glossaryservice/controller/GlossaryController.java
+++ glossary-service/src/main/java/com/example/glossaryservice/controller/GlossaryController.java
@@ -1,8 +1,27 @@
 package com.example.glossaryservice.controller;
 
+import com.example.glossaryservice.domain.Definition;
+import com.example.glossaryservice.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class GlossaryController {
 
+    @Autowired
+    ServiceLayer serviceLayer;
+
     // URL: /glossary/term/{term}
+    @RequestMapping(value = "/glossary/term/{term}", method = RequestMethod.GET)
+    public List<Definition> getDefinitionsByTerm(@PathVariable String term) {
+        return serviceLayer.getDefinitions(term);
+    }
 
     // URL: /glossary
+    @RequestMapping(value = "/glossary", method = RequestMethod.POST)
+    public Definition addDefinition(@RequestBody Definition defn) {
+        return serviceLayer.addDefinition(defn);
+    }
 }
